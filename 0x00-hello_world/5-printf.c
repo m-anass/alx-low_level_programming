260a96046e6af21287210212b1100b29e4dc2d7f
m-anass <manas8269@gmail.com>
2023-03-18 20:12:06 +0000

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
-
+/**
+ * main - entry point
+ * Descriotion: A C program that pritns put function
+ * Return: Alawys 0 (Success)
+ */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
 
