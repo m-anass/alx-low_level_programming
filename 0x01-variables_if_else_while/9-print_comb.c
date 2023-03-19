02b133d52a909d87c9181b3d7f6c5da0323034c5
m-anass <manas8269@gmail.com>
2023-03-19 19:56:40 +0000

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Descriotion: A C program that pritns put function
+ * Return: Alawys 0 (Success)
+ */
+int main(void)
+{
+	int x = 48;
+
+	while (x <= 57)
+	{
+		putchar(x);
+		putchar(',');
+		putchar(' ');
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
