4b0f7e54c5f685e1a476efed0e91ab39596a62d3
m-anass <manas8269@gmail.com>
2023-03-19 19:15:21 +0000

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Descriotion: A C program that pritns put function
+ * Return: Alawys 0 (Success)
+ */
+int main(void)
+{
+	int x = 97;
+	
+	while (x <= 122)
+	{
+		putchar(x);
+		x++;
+	}
+	return (0);
+}
