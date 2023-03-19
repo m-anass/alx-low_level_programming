fb53fa2aba62fe0bf99c26ce8669554d253f9439
m-anass <manas8269@gmail.com>
2023-03-19 19:41:01 +0000

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Descriotion: A C program that pritns put function
+ * Return: Alawys 0 (Success)
+ */
+int main(void)
+{
+	int x = 0;
+
+	while (x < 10)
+	{
+		printf("%d", x);
+		x++;
+	}
+	printf("\n");
+	return (0);
+}
