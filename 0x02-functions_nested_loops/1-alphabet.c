fa90ac223391fb4232c0d6dd9a4779411b95ac3f
m-anass <manas8269@gmail.com>
2023-03-21 17:04:24 +0000

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,17 @@
 #include "main.h"
-
+/**
+ * main - entry point
+ * Descriotion: A C program that pritns put function
+ * Return: Alawys 0 (Success)
+ */
 void print_alphabet(void)
 {
-        int x = 97;
+	int x = 97;
 
-        while (x <= 122)
-        {
-                _putchar(x);
-                x++;
-        }
-        _putchar('\n');
+	while (x <= 122)
+	{
+		_putchar(x);
+		x++;
+	}
+	_putchar('\n');
 }
