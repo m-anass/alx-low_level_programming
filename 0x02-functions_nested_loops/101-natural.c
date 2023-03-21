0e494be87d69f1a6c0b8c9f32343754a3260cfbd
m-anass <manas8269@gmail.com>
2023-03-21 21:08:04 +0000

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: klsavnld
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+	int sum = 0;
+
+	while (i < 1024)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			sum += i;
+		i++;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
