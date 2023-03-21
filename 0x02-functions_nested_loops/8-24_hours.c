3c2fb8fba66725b40661aab90fa88951f2f73731
m-anass <manas8269@gmail.com>
2023-03-21 22:21:19 +0000

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,6 +1,27 @@
 #include "main.h"
-
+/**
+ * jack_bauer - time
+ * Description: jdvn
+ * Return: void
+ */
 void jack_bauer(void)
 {
+	int hour = 0;
+	int min = 0;
 
+	while (hour < 24)
+	{
+		min = 0;
+		while (min < 60)
+		{
+			_putchar(hour / 10 + 48);
+			_putchar(hour % 10 + 48);
+			_putchar(':');
+			_putchar(min / 10 + 48);
+			_putchar(min % 10 + 48);
+			_putchar('\n');
+			min++;
+		}
+		hour++;
+	}
 }
