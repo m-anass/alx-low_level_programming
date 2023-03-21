3c608c6cf4b9cfeaa07e34c8fba76823e8399d57
m-anass <manas8269@gmail.com>
2023-03-21 16:58:18 +0000

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+void print_alphabet(void)
+{
+        int x = 97;
+
+        while (x <= 122)
+        {
+                _putchar(x);
+                x++;
+        }
+        _putchar('\n');
+}
