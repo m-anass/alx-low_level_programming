47312edee4c5d2cb937acf59f201fef6f91dfdc5
m-anass <manas8269@gmail.com>
2023-03-22 13:18:25 +0000

    0-main.c, main.h

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - entry point
+ * Description: lnknl
+ * Return: 0
+ */
+int main(void)
+{
+	positive_or_negative(0);
+	return (0);
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN
+#define MAIN
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+
+#endif
