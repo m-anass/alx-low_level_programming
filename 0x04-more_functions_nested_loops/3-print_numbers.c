8891450233fb6f287fe237f981fc57ed40525313
m-anass <manas8269@gmail.com>
2023-03-23 20:40:47 +0000

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int x = 48;
+
+	while (x <= 57)
+	{
+		_putchar(x);
+		x++;
+	}
+	_putchar('\n');
+}
