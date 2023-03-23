9a2fd17ee4f9c3b68f2f6a68f64c4dccc5fda2cb
m-anass <manas8269@gmail.com>
2023-03-23 20:54:13 +0000

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int x = 48;
+
+	while (x <= 57)
+	{
+		if (x == 50 || x == 52)
+		{
+			x++;
+			continue;
+		}
+		_putchar(x);
+		x++;
+	}
+	_putchar('\n');
+}
