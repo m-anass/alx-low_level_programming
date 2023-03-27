7e8c315d4e31c57e3aba99184dd201375ce06794
m-anass <manas8269@gmial.com>
2023-03-27 08:51:32 +0000

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * puts_half - print string
+ * @str: the string
+ */
+void puts_half(char *str)
+{
+	unsigned long int i = 0;
+	
+	if ((strlen(str) - 1) % 2 == 0)
+	{
+		i = (strlen(str) - 1) / 2;
+
+		while (i < strlen(str))
+		{
+			_putchar(str[i]);
+			i++;
+		}
+	}
+	else
+	{
+		i = (strlen(str) - 2) / 2;
+
+		while (i < strlen(str))
+		{
+			_putchar(str[i]);
+			i++;
+		}
+	}
+	_putchar('\n');
+}
