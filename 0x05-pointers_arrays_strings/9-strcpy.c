282dac3b41b84e6d363034cc1391ddbf5430c070
m-anass <manas8269@gmial.com>
2023-03-27 09:45:57 +0000

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,5 +1,10 @@
 #include "main.h"
-
+/**
+ * _strcpy - copy
+ * @dest: df
+ * @src: nlv
+ * Return: nl
+ */
 char *_strcpy(char *dest, char *src)
 {
 	strcpy(dest, src);
--- 0x05-pointers_arrays_strings/9-strcpy.c9-strcpy.c
+++
@@ -1,7 +0,0 @@
-#include "main.h"
-
-char *_strcpy(char *dest, char *src)
-{
-	strcpy(dest, src);
-	return (dest);
-}
