8367bddc64288a8d85ad1198aab3d176711471aa
m-anass <manas8269@gmial.com>
2023-03-27 08:58:46 +0000

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,7 +9,7 @@ void puts_half(char *str)
 
 	if ((strlen(str) - 1) % 2 == 0)
 	{
-		i = (strlen(str) - 1) / 2;
+		i = (strlen(str)) / 2;
 
 		while (i < strlen(str))
 		{
@@ -19,7 +19,7 @@ void puts_half(char *str)
 	}
 	else
 	{
-		i = (strlen(str) - 1) / 2;
+		i = (strlen(str) - 2) / 2;
 
 		while (i < strlen(str))
 		{
