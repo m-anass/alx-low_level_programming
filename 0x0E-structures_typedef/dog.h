7d8570929f40f4b0c353b29155470e5074733fc8
m-anass <manas8269@gmial.com>
2023-04-17 19:55:52 +0000

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Discription
+ * @name: joo
+ * @age: jl
+ * @owner: oo
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
